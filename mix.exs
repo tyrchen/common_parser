726c895294a966cbaf1a752a9da21a7e51a3505e
Tyr Chen <tyr.chen@gmail.com>
2019-05-03 11:20:56 -0700

    update deps and bump version

--- mix.exs
+++ mix.exs
@@ -18,7 +18,17 @@ defmodule Parser.MixProject do
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       dialyzer: [ignore_warnings: ".dialyzer_ignore.exs", plt_add_apps: [:nimble_parsec]],
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      # Docs
+      name: "CommonParser",
+      source_url: "https://github.com/tyrchen/common_parser",
+      homepage_url: "https://github.com/tyrchen/common_parser",
+      docs: [
+        main: "CommonParser",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -33,13 +43,38 @@ defmodule Parser.MixProject do
       {:nimble_parsec, "~> 0.5"},
 
       # dev & test
-      {:benchee, "~> 0.13", only: :dev},
-      {:benchee_html, "~> 0.4", only: :dev},
+      {:benchee, "~> 1.0", only: :dev},
+      {:benchee_html, "~> 1.0", only: :dev},
       {:credo, "~> 1.0.0", only: [:dev, :test]},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :integration], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :integration], runtime: false},
       {:ex_doc, "~> 0.19.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: [:test, :integration]},
       {:pre_commit_hook, "~> 1.2", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Multiple crypto support.
+    """
+  end
+
+  defp package do
+    [
+      files: [
+        "config",
+        "lib",
+        "mix.exs",
+        "README*",
+        "version",
+        ".elixir_version"
+      ],
+      licenses: ["Apache 2.0"],
+      maintainers: ["tyr.chen@gmail.com"],
+      links: %{
+        "GitHub" => "https://github.com/tyrchen/common_parser",
+        "Docs" => "https://hexdocs.pm/common_parser"
+      }
     ]
   end
 end
